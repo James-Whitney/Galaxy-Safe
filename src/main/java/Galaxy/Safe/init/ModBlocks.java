639a25e5ac6046b52e7cce6541ca5bb1cc12c76e
James-Whitney <jwhitn01@calpoly.edu>
2016-09-03 22:19:26 -0700

    4

---
+++ src/main/java/Galaxy/Safe/init/ModBlocks.java
@@ -0,0 +1,42 @@
+package Galaxy.Safe.init;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModBlocks {
+	
+	//One for each block
+	public static Block chest;
+	
+	public static void init() {
+		//One for each block
+		cheese = new BlockChest();
+	}
+	
+	public static void register() {
+		//One for each block
+		registerBlock(cheese);
+		registerBlock(jar);
+	}
+	
+	private static void registerBlock(Block block) {
+		GameRegistry.register(block);
+		ItemBlock item = new ItemBlock(block);
+		item.setRegistryName(block.getRegistryName());
+		GameRegistry.register(item);
+	}
+	
+	public static void registerRenders() {
+		//One for each block
+		registerRender(cheese);
+		registerRender(jar);
+	}
+	
+	private static void registerRender(Block block) {
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
+	}
+}
