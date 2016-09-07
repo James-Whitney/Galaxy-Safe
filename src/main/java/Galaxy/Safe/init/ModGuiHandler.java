a059000c87d7ebd0ef5ee92fd0f8777e6be20fb3
James-Whitney <jwhitn01@calpoly.edu>
2016-09-06 19:17:08 -0700

    1

---
+++ src/main/java/Galaxy/Safe/init/ModGuiHandler.java
@@ -0,0 +1,25 @@
+package Galaxy.Safe.init;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.IGuiHandler;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+
+public class ModGuiHandler implements IGuiHandler {
+/*
+	public static void register() {
+		//One for each block
+	NetworkRegistry.INSTANCE.registerGuiHandler(Main.instance, new ModGuiHandler());
+	}*/
+	
+	
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+}
